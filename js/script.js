80d3432cb77d8929f48e4cdc4f8d67ef3dd8f4a8
Itoshuga <38258159+Itoshuga@users.noreply.github.com>
2021-04-20 00:07:09 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('nav .nav-btn').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    })
+});
+
