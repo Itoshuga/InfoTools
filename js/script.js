76f8c637e96ce8a86d5f08fc48bdd7df81046059
Itoshuga <38258159+Itoshuga@users.noreply.github.com>
2021-04-20 00:06:16 +0200

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-
-$(document).ready(function(){
-    // toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('nav .nav-btn').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    })
-});
-
